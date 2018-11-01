f9b853e229bb97340e938ed7f0b1147352f8c56e
Dj Lukis.LT <lukis101@gmail.com>
2018-11-01 12:10:56 +0200

    Update unmerged goblin factory

--- GameServer/EnemyFactory/GoblinFactory.cs
+++ GameServer/EnemyFactory/GoblinFactory.cs
@@ -2,10 +2,28 @@
  * @(#) GoblinFactory.cs
  */
 
+using System;
+
 namespace GameServer.EnemyFactory
 {
-	public class GoblinFactory : EnemyFactory
-	{
-	}
-	
+    public class GoblinFactory : EnemyFactory
+    {
+        public override Entity createGoblin(int difficultyLevel)
+        {
+            Entity enemy;
+            const int ATT = 2;
+            const int DEF = 4;
+
+            Random rand = new Random();
+            int type = rand.Next(0, 1);
+            switch (type)
+            {
+                default:
+                case 0: enemy = new Enemies.GoblinRogue("Goblin Rogue", 10, ATT, DEF); break;
+                case 1: enemy = new Enemies.GoblinWarrior("Goblin Warrior", 20, ATT, DEF); break;
+            }
+            return enemy;
+        }
+    }
+
 }
