3936083d0ed943379c124d84cfae1ac1739be70d
DomantasZ <domantaszavadskis@gmail.com>
2018-11-02 11:49:52 +0200

    Director update

--- GameServer/EnemyBuilder/EnemyPartyDirector.cs
+++ GameServer/EnemyBuilder/EnemyPartyDirector.cs
@@ -5,66 +5,22 @@ using GameServer.Models;
 
 namespace GameServer.EnemyBuilder
 {
-	public class EnemyPartyDirector
-	{
-        private Random random = new Random();
+    public class EnemyPartyDirector
+    {
         public EnemyPartyDirector(EnemyFactory.EnemyFactory factory) => builder = new EnemyPartyBuilder(factory);
 
         private IEnemyPartyBuilder builder;
-		
-		public EnemyParty construct(int difficultyLevel, int enemyCnt, Coordinates p)
-		{
+
+        public EnemyParty construct(int difficultyLevel, int enemyCnt, Coordinates p)
+        {
             builder.StartNew(p);
             for (int i = 0; i < enemyCnt; i++)
             {
-                int r = random.Next(100);
-                if (difficultyLevel < 10)
-                {
-                    if(r < 50)
-                    {
-                        builder.AddEnemy(0);
-                    } else if(r < 85)
-                    {
-                        builder.AddEnemy(1);
-                    } else
-                    {
-                        builder.AddEnemy(2);
-                    }
-                }
-                else if (difficultyLevel < 50)
-                {
-                    if (r < 25)
-                    {
-                        builder.AddEnemy(0);
-                    }
-                    else if (r < 70)
-                    {
-                        builder.AddEnemy(1);
-                    }
-                    else
-                    {
-                        builder.AddEnemy(2);
-                    }
-                }
-                else
-                {
-                    if (r < 15)
-                    {
-                        builder.AddEnemy(0);
-                    }
-                    else if (r < 40)
-                    {
-                        builder.AddEnemy(1);
-                    }
-                    else
-                    {
-                        builder.AddEnemy(2);
-                    }
-                }
+                builder.AddEnemy(difficultyLevel++);
             }
             return builder.Build();
-		}
-		
-	}
-	
+        }
+
+    }
+
 }
